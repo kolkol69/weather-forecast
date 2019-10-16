95f5e66a73cf903d4d9964ed70ac84c66f69448a
Student <student@imac-07.local>
2019-10-16 15:09:04 +0200

    Initial Commit

---
+++ kolkol_weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kolkol_weather
+//
+//  Created by Student on 16/10/2019.
+//  Copyright © 2019 mkolodiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
