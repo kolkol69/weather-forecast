8101498388330ee687a9ca8b0667781069c84391
kolkol <kolodiy1103@gmail.com>
2019-11-19 23:33:30 +0100

    Read previously removed icons/photos

--- kolkol_weather/Controllers/ViewController.swift
+++ kolkol_weather/Controllers/ViewController.swift
@@ -120,6 +120,21 @@ class ViewController: UIViewController {
             self.navigationItem.title = self.CityLbl
         }
         self.styleHeader()
+        
+//        let formatter = DateFormatter()
+//        // initially set the format based on your datepicker date / server String
+//        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+//
+//        let myString = formatter.string(from: Date()) // string purpose I add here
+//        // convert your string to date
+//        let yourDate = formatter.date(from: myString)
+//        //then again set the date format whhich type of output you need
+//        formatter.dateFormat = "dd-MMM-yyyy"
+//        // again convert your date to string
+//        let myStringafd = formatter.string(from: yourDate!)
+//
+//        print(myStringafd)
+//        MyName.text = myStringafd
         MyName.text = "Maksym Kolodiy"
         self.disableButton(button: self.PrevDayButton)
         fetchData()
